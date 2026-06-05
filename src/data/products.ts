bc06355d62537ff34cf00163d52e041b820aad74
Sultan <osultan@yandex.com>
2026-06-05 18:30:06 +0500

    добавил изображения продукции

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,90 +4,98 @@ export const products: Product[] = [
   {
     id: '1',
     name: 'Мёд Липовый',
-    description: 'Светлый, ароматный мёд с нежным вкусом. Собран в период цветения липы.',
+    description:
+      'Светлый, ароматный мёд с нежным вкусом. Собран в период цветения липы.',
     price: 850,
     category: 'honey',
     image: '/images/products/honey-linden.png',
     isPopular: true,
     inStock: true,
-    weight: '500г'
+    weight: '500г',
   },
   {
     id: '2',
     name: 'Мёд Гречишный',
-    description: 'Терпкий, насыщенный мёд темного цвета с высоким содержанием железа.',
+    description:
+      'Терпкий, насыщенный мёд темного цвета с высоким содержанием железа.',
     price: 750,
     category: 'honey',
     image: '/images/products/honey-buckwheat.png',
     isPopular: true,
     inStock: true,
-    weight: '500г'
+    weight: '500г',
   },
   {
     id: '3',
     name: 'Мёд Цветочный (Разнотравье)',
-    description: 'Собран с различных луговых цветов. Обладает богатым букетом ароматов.',
+    description:
+      'Собран с различных луговых цветов. Обладает богатым букетом ароматов.',
     price: 650,
     category: 'honey',
     image: '/images/products/honey-flower.png',
     isPopular: false,
     inStock: true,
-    weight: '500г'
+    weight: '500г',
   },
   {
     id: '4',
     name: 'Прополис натуральный',
-    description: 'Природный антибиотик. Обладает мощными бактерицидными свойствами.',
+    description:
+      'Природный антибиотик. Обладает мощными бактерицидными свойствами.',
     price: 350,
     category: 'propolis',
     image: '/images/products/propolis.png',
     isPopular: true,
     inStock: true,
-    weight: '50г'
+    weight: '50г',
   },
   {
     id: '5',
     name: 'Перга пчелиная',
-    description: '«Пчелиный хлеб». Уникальный витаминный комплекс, созданный самой природой.',
+    description:
+      '«Пчелиный хлеб». Уникальный витаминный комплекс, созданный самой природой.',
     price: 1200,
     category: 'perga',
     image: '/images/products/perga.png',
     isPopular: true,
     inStock: true,
-    weight: '100г'
+    weight: '100г',
   },
   {
     id: '6',
     name: 'Пчелиный воск',
-    description: 'Натуральный очищенный воск для изготовления свечей и косметики.',
+    description:
+      'Натуральный очищенный воск для изготовления свечей и косметики.',
     price: 400,
     category: 'wax',
     image: '/images/products/wax.png',
     isPopular: false,
     inStock: true,
-    weight: '200г'
+    weight: '500г',
   },
   {
     id: '7',
     name: 'Мёд в сотах',
-    description: 'Мёд в первозданном виде, запечатанный пчелами. Самый полезный продукт.',
+    description:
+      'Мёд в первозданном виде, запечатанный пчелами. Самый полезный продукт.',
     price: 950,
     category: 'honey',
     image: '/images/products/honey-comb.png',
     isPopular: false,
     inStock: true,
-    weight: '300г'
+    weight: '500г',
   },
   {
     id: '8',
     name: 'Пыльца цветочная',
-    description: 'Кладезь аминокислот и микроэлементов. Отличное укрепляющее средство.',
+    description:
+      'Кладезь аминокислот и микроэлементов. Отличное укрепляющее средство.',
     price: 550,
     category: 'perga',
     image: '/images/products/pollen.png',
     isPopular: false,
     inStock: true,
-    weight: '100г'
+    weight: '100г',
   },
   {
     id: '9',
@@ -98,7 +106,7 @@ export const products: Product[] = [
     image: '/images/products/gift-set.png',
     isPopular: true,
     inStock: true,
-    weight: '1500г'
+    weight: '1500г',
   },
   {
     id: '10',
@@ -109,6 +117,6 @@ export const products: Product[] = [
     image: '/images/products/candles.png',
     isPopular: false,
     inStock: true,
-    weight: '2 шт'
-  }
-];
+    weight: '2 шт',
+  },
+]
